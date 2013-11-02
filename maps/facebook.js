2cb87dd9bf1f50ea6df1fd34e9895d6d33ae9a27
Glenn Jones <glennjonesnet@gmail.com>
2013-11-02 13:37:28 +0000

    Corrected uppercase issue stopping Nodejitsu deploys

--- maps/facebook.js
+++ maps/facebook.js
@@ -1,16 +1,35 @@
 {
     "name": "Facebook",
     "domain": "facebook.com",
+    "altDomainFilter": ".facebook.",
+    "altDomains": ["m.facebook.com","www.facebook.es", "www.facebook.se"],
     "urlMappings": [{
         "urlTemplate": "http://facebook.com/{username}",
         "schema": "None",
         "contentType": "Profile",
-        "mediaType": "html"
+        "mediaType": "Html"
     }, {
-        "urlTemplate": "http://facebook.com/people/{username}/{userid}",
+        "urlTemplate": "http://facebook.com/people/{username}/",
+        "schema": "OpenGraph",
+        "contentType": "Profile",
+        "mediaType": "Html"
+    }, {
+        "urlTemplate": "http://facebook.com/pages/{username}/",
+        "schema": "OpenGraph",
+        "contentType": "None",
+        "mediaType": "Html"
+    }, {
+        "urlTemplate": "http://facebook.com/{username}/posts/{*}",
         "schema": "None",
         "contentType": "None",
-        "mediaType": "html"
+        "mediaType": "Html"
     }],
-    "www": true
+    "www": true,
+    "blockList": [
+        "https://www.facebook.com/photo.php",
+        "https://www.facebook.com/login.php",
+        "https://www.facebook.com/permalink.php",
+        "https://www.facebook.com/story.php",
+        "https://www.facebook.com/album.php"
+    ]
 }
